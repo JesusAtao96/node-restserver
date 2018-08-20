7b33d5de26767bbfa70c81dc8b4370c14ca3d735
Jesús Alejandro Atao <jealeat96@gmail.com>
2018-08-20 18:17:50 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds125932.mlab.com:25932/jealeat-cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
